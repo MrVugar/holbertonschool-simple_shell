57d9edd1a751a32ba2034f5a66e0214f3d707ce2
MrVugar <vuqarazizov7777@gmail.com>
2024-12-16 04:44:07 +0000

    task

---
+++ shell.h
@@ -0,0 +1,14 @@
+#ifndef SHELL_HEADER
+#define SHELL_HEADER
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+extern char **environ;
+
+void print_env(void);
+
+#endif
